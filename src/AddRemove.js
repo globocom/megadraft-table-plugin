359711d88d5845267a43f59d61c3052f88027e1f
siminino <victor.areas@corp.globo.com>
2017-04-13 11:33:08 -0300

    alterando AddRemove component to use bsui style

--- src/AddRemove.js
+++ src/AddRemove.js
@@ -11,9 +11,9 @@ import {FormItem} from "./FormComponents";
 export function AddRemove({title, onAdd, onRemove}) {
   return (
     <FormItem>
-      <label>{title}</label>
-      <button className="btn-add" onClick={onAdd}>+ Adicionar</button>
-      <button className="btn-remove" onClick={onRemove}>X Remover</button>
+      <label className="bs-ui-input__label">{title}</label>
+      <button className="bs-ui-button btn-add" onClick={onAdd}>+ Adicionar</button>
+      <button className="bs-ui-button btn-remove" onClick={onRemove}>X Remover</button>
     </FormItem>
   );
 }
