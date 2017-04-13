bd016801b95d6ac1dfca730a214dcf5a5e9c1bfc
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2017-04-13 10:18:10 -0300

    Adding buildColumns and buildRows method to TableView.

--- src/TableView.js
+++ src/TableView.js
@@ -6,16 +6,38 @@ export default class TableView extends Component {
 
   constructor(props) {
       super(props);
-
+      let columns = this.buildColumns(props.rows);
+      let rows = this.buildRows(props.rows, columns)
       this.state = {
-          columns: [{property: "c1" }],
-          rows: [{c1: "00"}]
+          columns: columns,
+          rows: rows
       };
 
   }
 
-    render() {
-        return (<h1></h1>
-            );
+  buildColumns(rows) {
+    let columns = []
+    for(let i=0; i < rows[0].length; i++) {
+      let propertyName = "c" + i;
+      columns.push({property: propertyName});
     }
+    return columns;
+  }
+
+  buildRows(rows, columns) {
+    let newRows = [];
+    for(let i=0 ; i< rows.length; i++) {
+      let row = {};
+      for(let j=0; j < rows[i].length; j++) {
+        row[columns[j].property] = rows[i][j];
+      }
+      newRows.push(row);
+    }
+    return newRows;
+  }
+
+  render() {
+      return (<h1></h1>
+          );
+  }
 }
--- tests/TableView_test.js
+++ tests/TableView_test.js
@@ -18,15 +18,41 @@ import TableView from "../src/TableView";
 const expect = chai.expect;
 
 describe("TableView", function() {
+    const rowsWithOneCell = [["00"]];
+    const rowsWithTwoCells = [["00"],["10"]];
+    const firstColumnName = "c0";
+
+    const createTable = function(rows){
+        return mount(<TableView rows={rows}/>);
+    }
+
     it("should have one column", function() {
-      const rows = [["00"]];
-      let tableView = mount(<TableView rows={rows}/>);
-      expect(tableView.instance().state.columns.length).to.be.equal(rows[0].length);
+      let tableView = createTable(rowsWithOneCell);
+      expect(tableView.instance().state.columns.length).to.be.equal(rowsWithOneCell[0].length);
     });
 
     it("should have one row", function() {
-      const rows = [["00"]];
-      let tableView = mount(<TableView rows={rows}/>);
-      expect(tableView.instance().state.rows.length).to.be.equal(rows[0].length);
+      let tableView = createTable(rowsWithOneCell);
+      expect(tableView.instance().state.rows.length).to.be.equal(rowsWithOneCell[0].length);
+    });
+
+    it("should have one cell value", function() {
+      let tableView = createTable(rowsWithOneCell);
+      expect(tableView.instance().state.rows[0][firstColumnName]).to.be.equal(rowsWithOneCell[0][0]);
+    });
+
+    it("should have column property value with same name as row key", function() {
+      const propertyName = firstColumnName;
+      let tableView = createTable(rowsWithOneCell);
+      expect(tableView.instance().state.rows[0]).to.have.property(propertyName);
+      expect(tableView.instance().state.columns[0].property).to.be.equal(propertyName);
+    });
+
+    it("should have two rows with one column", function() {
+      let tableView = createTable(rowsWithTwoCells);
+      for(let i=0; i<rowsWithTwoCells.length ;i++) {
+        expect(tableView.instance().state.rows[i][firstColumnName]).to.be.equal(rowsWithTwoCells[i][0]);
+      }
+      expect(tableView.instance().state.columns).to.have.lengthOf(rowsWithTwoCells[0].length);
     });
 })
