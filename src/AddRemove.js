d24bef6778e9d35e64883246677420e08157cd8a
siminino <victor.areas@corp.globo.com>
2017-04-18 18:15:11 -0300

    add svg icon close and plus

--- src/AddRemove.js
+++ src/AddRemove.js
@@ -7,13 +7,18 @@
 import React from "react";
 
 import {FormItem} from "./FormComponents";
+import {PlusIcon, CloseIcon} from "./Icons";
 
 export function AddRemove({title, className, onAdd, onRemove}) {
   return (
     <FormItem className={className}>
       <label className="bs-ui-input__label">{title}</label>
-      <button className="bs-ui-button bs-ui-button--small bs-ui-button--blue btn-add" onClick={onAdd}>+ Adicionar</button>
-      <button className="bs-ui-button bs-ui-button--small bs-ui-button--red btn-remove" onClick={onRemove}>X Remover</button>
+      <button className="bs-ui-button bs-ui-button--small bs-ui-button--blue btn-add" onClick={onAdd}>
+        <PlusIcon /> Adicionar
+      </button>
+      <button className="bs-ui-button bs-ui-button--small bs-ui-button--red btn-remove" onClick={onRemove}>
+        <CloseIcon /> Remover
+      </button>
     </FormItem>
   );
 }
--- src/Icons.js
+++ src/Icons.js
@@ -24,3 +24,19 @@ export function HighlightIcon({className = ""}) {
     </svg>
   );
 }
+
+export function PlusIcon({className = ""}) {
+  return (
+    <svg className={className} width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+      <path fill="#000000" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+    </svg>
+  );
+}
+
+export function CloseIcon({className = ""}) {
+  return (
+    <svg className={className} width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+      <path fill="#000000" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+    </svg>
+  );
+}
