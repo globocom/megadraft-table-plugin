4c4a65596a72ad0b545b6d711f3b45c60f49c14c
Diego Manhães Pinheiro <diego.pinheiro@corp.globo.com>
2017-05-09 14:00:27 -0300

    Moving header styler buttons next to the add/remove lines and rows.

--- src/TableManagerModal.js
+++ src/TableManagerModal.js
@@ -12,6 +12,7 @@ import TableView from "./TableView";
 import {TableConfig, validate} from "./TableConfig";
 import {TableManagerActions} from "./TableManagerActions";
 import {TableManagerMetadata} from "./TableManagerMetadata";
+import {HeaderStyle} from "./HeaderStyle";
 
 
 export default class TableManagerModal extends Component {
@@ -89,14 +90,17 @@ export default class TableManagerModal extends Component {
               data={data}
               errors={errors} />
 
-            {/* Metadado */}
-
             {/* <Acoes onChangeRows selectedCell rows /> */}
 
             <TableManagerActions
               onChangeRows={this.onChangeRows}
               selectedCell={this.state.selectedCell}
               rows={data.rows} />
+
+            <HeaderStyle name="headerStyle"
+              selectedOptions={data.headerStyle}
+              onChange={this.onFormItemChange}/>
+
 
             {/**/}
 
