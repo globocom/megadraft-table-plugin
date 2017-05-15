cf95b0fc9e79df60b6fe6ecaf9848add160ec142
Diego Manhães Pinheiro <diego.pinheiro@corp.globo.com>
2017-05-15 11:26:28 -0300

    Triggering jquery call on componentDidMount function.

---
+++ src/Tooltip.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017, Globo.com <https://github.com/globocom/megadraft-table-plugin>
+ *
+ * License: MIT
+ */
+
+import React, {Component} from "react";
+
+export default class TableManagerModal extends Component {
+
+  render() {
+      return <span className="jsonform-description header-style__tooltip" data-toggle="tooltip" title="Linha e/ou coluna destacada de uma tabela"></span>
+  }
+
+  componentDidMount() {
+      if (window.$ && window.$.fn && window.$.fn.tooltip) {
+        window.$(".header-style__tooltip").tooltip();
+      }
+  }
+
+}
